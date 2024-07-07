6a7182be453b16be154f396f1f895cde67fdcd37
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2024-07-07 17:58:39 +0300

    feat: events fetching

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,12 +1,107 @@
 import styles from "./Calendar.module.scss"
 import {CalendarWeekday} from "../CalendarWeekday/CalendarWeekday.tsx";
 import {CalendarDay} from "../CalendarDay/CalendarDay.tsx";
+import {useContext, useEffect, useState} from "react";
+import axios from "axios";
+import qs from "qs";
+import {DateContext, DateContextType} from "../../contexts/DateContext.tsx";
 
 export function Calendar() {
     const weekDays = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"]
-    const days = ["30", "31", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    const dateContext = useContext(DateContext) as DateContextType
+
+    const [days, setDays] = useState<Array<Date>>([]);
+    const [error, setError] = useState(null);
+    const [events, setEvents] = useState<Array<any>>([]);
+
+    useEffect(() => {
+        const dates: Array<Date> = []
+        const firstDay = new Date(dateContext.selectedYear, dateContext.selectedMonth)
+        for (let i = (firstDay.getDay() + 6) % 7; i > 0; i--) {
+            const dateToPrepend = new Date(firstDay)
+            dateToPrepend.setDate(dateToPrepend.getDate() - i)
+            dates.push(dateToPrepend)
+        }
+        const amountOfDatesToAdd = 42 - dates.length
+        for (let i = 0; i < amountOfDatesToAdd; i++) {
+            const dateToAdd = new Date(firstDay)
+            dateToAdd.setDate(dateToAdd.getDate() + i)
+            dates.push(dateToAdd)
+        }
+        setDays(dates)
+    }, [dateContext.selectedMonth, dateContext.selectedYear])
+
+    useEffect(() => {
+        if (days.length == 0) return
+        const pastLimit = days[0]
+        const futureLimit = days[days.length - 1]
+        futureLimit.setDate(futureLimit.getDate() + 1)
+
+        const query = qs.stringify({
+            filters: {
+                $or: [
+                    {
+                        $and: [
+                            {
+                                dateEnd: {
+                                    $gte: pastLimit
+                                }
+                            },
+                            {
+                                dateEnd: {
+                                    $lt: futureLimit
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        $and: [
+                            {
+                                dateStart: {
+                                    $gte: pastLimit
+                                }
+                            },
+                            {
+                                dateStart: {
+                                    $lt: futureLimit
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        })
+
+        console.log(pastLimit)
+        console.log(futureLimit)
+        const request = `https://planner.rdclr.ru/api/events?${query}`
+        console.log(request)
+
+        axios.get(request)
+            .then((response) => {
+                console.log(response.data.data)
+                setEvents(response.data.data)
+            })
+            .catch((error) => setError(error))
+    }, [days])
+
     const weekDaysComponents = weekDays.map(day => <CalendarWeekday day={day}/>)
-    const daysComponents = days.map((day, index) =><CalendarDay day={day} weekend={index % 7 - 6 == 0 || index % 7 - 5 == 0}  currentMonth={index > 1 && index < 33} />)
+    const daysComponents = days.map(
+        (day, index) => {
+            return <CalendarDay
+                day={day}
+                events={
+                    events.filter(x => {
+                        const start = new Date(day)
+                        start.setHours(0, 0, 0, 0)
+                        const end = new Date(day)
+                        end.setHours(23, 59, 59, 999)
+                        return Date.parse(x.dateStart) <= end.getTime() && Date.parse(x.dateEnd) >= start.getTime()
+                    })}
+                weekend={index % 7 - 6 == 0 || index % 7 - 5 == 0}
+                currentMonth={day.getMonth() == dateContext.selectedMonth}
+            />
+        })
 
     return (
         <div className={styles.calendar}>
--- src/components/CalendarDay/CalendarDay.tsx
+++ src/components/CalendarDay/CalendarDay.tsx
@@ -2,20 +2,23 @@ import styles from "./CalendarDay.module.scss"
 import {CalendarDayEvent} from "../CalendarDayEvent/CalendarDayEvent.tsx";
 
 interface CalendarGridWeekdayProps {
-    day: string
+    day: Date
+    events: Array<any>,
     currentMonth?: boolean,
     weekend?: boolean
 }
 
-export function CalendarDay({day, currentMonth, weekend}: CalendarGridWeekdayProps) {
+export function CalendarDay({day, events, currentMonth, weekend}: CalendarGridWeekdayProps) {
     const currentMonthStyle = currentMonth ? styles.currentMonth : "";
     const weekendStyle = weekend ? styles.weekend : "";
+
+    const eventComponents = events.map(x=> <CalendarDayEvent label={x.title} type={Date.now() > Date.parse(x.dateEnd) ? "past" : "future"}/>)
+
     return (
         <div className={`${styles.day} ${weekendStyle} ${currentMonthStyle}`}>
-            <p className={`${styles.date} ${currentMonthStyle}`}>{day}</p>
+            <p className={`${styles.date} ${currentMonthStyle}`}>{day.getDate()}</p>
             <div className={styles.events}>
-                <CalendarDayEvent label={"Музыкальный опен-эйр фестиваль"} type={"future"}></CalendarDayEvent>
-                <CalendarDayEvent label={"Музыкальный опен-эйр фестиваль"} type={"future"}></CalendarDayEvent>
+                {eventComponents}
             </div>
         </div>
     )
