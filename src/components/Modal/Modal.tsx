9cda3dd47ea5b821ed4ca11d63aaff01f1dd2f86
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2024-07-13 07:41:39 +0300

    feat: Modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,32 @@
+import styles from "../Modal/Modal.module.scss";
+import {ReactNode, useEffect, useRef} from "react";
+import {IconCross} from "../Icons/IconCross.tsx";
+
+interface ModalProps {
+    label?: string,
+    open: boolean,
+    onClose?: () => void,
+    children: ReactNode | ReactNode[]
+}
+
+export function Modal({label, open, onClose, children}: ModalProps) {
+    const ref = useRef<HTMLDialogElement>(null);
+
+    useEffect(() => {
+        if (open)
+            ref.current?.showModal();
+        else{
+            ref.current?.close();
+        }
+    }, [open]);
+
+    return (
+        <dialog className={styles.dialog} ref={ref} onClose={onClose}>
+            <button className={styles.close} onClick={onClose}>
+                <IconCross size={"1.375rem"}></IconCross>
+            </button>
+            {label ? <h2 className={styles.label}>{label}</h2> : ""}
+            {children}
+        </dialog>
+    )
+}
