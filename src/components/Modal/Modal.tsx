8e63726ae6d4771d4e6f4f32b95818ee36ff8241
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2024-07-13 17:34:59 +0300

    feat: added className to ModalProps

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -6,22 +6,23 @@ interface ModalProps {
     label?: string,
     open: boolean,
     onClose?: () => void,
+    className?: string
     children: ReactNode | ReactNode[]
 }
 
-export function Modal({label, open, onClose, children}: ModalProps) {
+export function Modal({label, open, onClose, className, children}: ModalProps) {
     const ref = useRef<HTMLDialogElement>(null);
 
     useEffect(() => {
         if (open)
             ref.current?.showModal();
-        else{
+        else {
             ref.current?.close();
         }
     }, [open]);
 
     return (
-        <dialog className={styles.dialog} ref={ref} onClose={onClose}>
+        <dialog className={`${styles.dialog} ${className}`} ref={ref} onClose={onClose}>
             <button className={styles.close} onClick={onClose}>
                 <IconCross size={"1.375rem"}></IconCross>
             </button>
