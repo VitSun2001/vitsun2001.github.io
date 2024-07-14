837ae2afa2dd21ff42b72799622edc69a3f281bb
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2024-07-15 01:47:23 +0300

    build: deploy branch

--- src/components/Icons/IconCircle.tsx
+++
@@ -1,9 +0,0 @@
-import {IconProps} from "./IconProps.ts";
-
-export function IconCircle({size = "1rem", color = "currentColor"}: IconProps) {
-    return (
-        <svg width={size} height={size} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="16" cy="16" r="16" fill={color}/>
-        </svg>
-    );
-}
