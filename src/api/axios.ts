c7b1fa5a0853ac6cefc24c02f97eefaf0b11e2bc
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2024-07-15 19:09:33 +0300

    fix: remove token from localstorage on 401

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,15 +1,30 @@
-import axios from "axios";
+import axios, {HttpStatusCode} from "axios";
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
 })
 
-axiosInstance.interceptors.request.use((config) =>{
-    if(localStorage.getItem("jwt")){
+axiosInstance.interceptors.request.use((config) => {
+    if (localStorage.getItem("jwt")) {
         config.headers.Authorization = `Bearer ${localStorage.getItem("jwt")}`
     }
     config.headers["Content-Type"] = "application/json"
     return config
 })
+
+axiosInstance.interceptors.response.use((response) => response,
+    async function (error) {
+        const originalRequest = error?.config
+        const status = error?.response?.status
+
+        if ((status === HttpStatusCode.Unauthorized) && !originalRequest?.retry) {
+            originalRequest.retry = true
+            localStorage.removeItem("jwt")
+            originalRequest.headers.Authorization = undefined
+            return axiosInstance(originalRequest)
+        }
+        return Promise.reject(error)
+    }
+)
 
 export default axiosInstance
